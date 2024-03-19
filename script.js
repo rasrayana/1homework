7adf55ecb782671e4169dae7e999a8902b052b4a
Rayana <rasyanara208@gmail.com>
2024-03-19 23:51:23 +0600

    abbys

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('nextButton').addEventListener('click', function(){
+    window.location.href = 'screamer.html';
+    var photo = document.getElementById('img\mainyukie.gif');
+    photo.src = photo;
+});
